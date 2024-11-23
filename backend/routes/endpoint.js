74a53ddc0517ef986257f6355f1bcc2dd3646154
Juan Esteban Perea Olivares <jeperea45@gmail.com>
2024-11-23 17:25:36 -0500

    Fixed Controllers in the Routes

--- backend/routes/endpoint.js
+++ backend/routes/endpoint.js
@@ -3,9 +3,10 @@ const router = express.Router();
 // controllador
 const mainController = require("../controllers/mainController");
 const actividadesController = require("../controllers/actividadesController")
+const proyectosController = require("../controllers/proyectosController")
 
 // agrega los proyectos
-router.post("/createProyect");
+router.post("/createProyect", proyectosController.crearProyecto);
 // obtiene las actividades relacionadas a un proyecto
 router.get("/readActivity/:proyecto_id", mainController.obtenerProyectosConActividades);
 // agrega las actividades relacionadas a un proyecto el cual el id se pasa por body
